309657580af7adc38922d46622517cef01eda0a0
Beka <ateneya@outlook.com>
2025-10-23 09:29:33 +0500

    First commit

---
+++ CalculatoriBrat/Class/Calcutaorcs.cs
@@ -0,0 +1,33 @@
+﻿using CalculatoriBrat.Class.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatoriBrat.Class
+{
+    internal class Calcutaorcs : ICalculatorBasic
+    {
+        public double Addition(double valuel, double valuel2)
+        {
+            return valuel + valuel2;
+        }
+
+     
+        public double Division(double valuel, double valuel2)
+        {
+            return valuel / valuel2;
+        }
+
+        public double Multiplication(double valuel, double valuel2)
+        {
+            return valuel * valuel2;
+        }
+
+        public double Substraction(double valuel, double valuel2)
+        {
+            return valuel - valuel2;
+        }
+    }
+}
---
+++ CalculatoriBrat/Class/Interface/ICalculatorBasic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatoriBrat.Class.Interface
+{
+    public interface ICalculatorBasic
+    {
+        public double Addition(double valuel, double valuel2);
+        public double Substraction(double valuel, double valuel2);
+        public double Multiplication(double valuel, double valuel2);
+        public double Division(double valuel, double valuel2);
+    }
+}
+    
